cf5914701a829652e6d57848c97065c8acfcb034
lostryn <rayyan200819@gmail.com>
2025-08-15 17:05:57 +0530

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("generateBtn").addEventListener("click", function() {
-    document.getElementById("status").textContent = "Generating your reel... (feature coming soon)";
-});
